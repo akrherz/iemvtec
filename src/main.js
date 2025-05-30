b3749899d91736b67036d720c48e8e65d16971e1
akrherz <akrherz@iastate.edu>
2025-05-29 22:35:53 -0500

    ⏪️ Revert to bootstrap 4

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import 'ol/ol.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { Style, Icon, Stroke, Circle, Fill, Text } from 'ol/style';
